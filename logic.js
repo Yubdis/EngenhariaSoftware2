585ee1a4372cfb5cc17f88dbb16fdb2d8790353a
Simon <simontsui85@gmail.com>
2024-04-21 20:36:11 -0300

    fixed hover bug

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 // Define variables
-const box1 = document.getElementById("box1")
-const box2 = document.getElementById("box2")
+const blueBox = document.querySelector("#blueBox")
+const redBox = document.querySelector("#redBox")
 const possibleButton = document.getElementById("possible")
 const impossibleButton = document.getElementById("impossible")
 
@@ -8,47 +8,62 @@ const impossibleButton = document.getElementById("impossible")
 const initialState = {
     state: 'possible' // Initial state
   };
-  
+
 // Create a class for the state context
   class StateContext {
     constructor() {
       this.state = initialState;
     }
-  
+
     // Method to change the state
     changeState(newState) {
       this.state = { ...this.state, state: newState };
       console.log('State changed to ', this.state)
     }
-  
+
     // Method to handle box click
     handleBoxClick() {
       if (this.state.state === 'possible') {
         console.log('Box clicked!');
+
       }
     }
-  
+
     // Method to handle box hover
-    handleBoxHover() {
+    handleBlueBoxHover() {
       if (this.state.state === 'impossible') {
         // Hide the box
-        document.querySelector('.box').style.display = 'none';
+        blueBox.classList.add('hidden');
       }
     }
+    handleRedBoxHover() {
+      if (this.state.state === 'impossible') {
+        // Hide the box
+        redBox.classList.add('hidden');
+      }
+    }
+    handleBlueBoxMouseOut() {
+      blueBox.classList.remove('hidden');
+    }
+    handleRedBoxMouseOut() {
+      redBox.classList.remove('hidden');
+    }
   }
-  
+
   // Initialize the state context
   const stateContext = new StateContext();
 
 
-  box1.addEventListener('click', () => stateContext.handleBoxClick());
-  box1.addEventListener('mouseover', () => stateContext.handleBoxHover());
-  
-  box2.addEventListener('click', () => stateContext.handleBoxClick());
-  box2.addEventListener('mouseover', () => stateContext.handleBoxHover());
-  
+  blueBox.addEventListener('mouseover', () => stateContext.handleBlueBoxHover());
+  blueBox.addEventListener('mouseout', () => stateContext.handleBlueBoxMouseOut());
+  blueBox.addEventListener('click', () => stateContext.handleBoxClick());
+
+  redBox.addEventListener('mouseover', () => stateContext.handleRedBoxHover());
+  redBox.addEventListener('mouseout', () => stateContext.handleRedBoxMouseOut());
+  redBox.addEventListener('click', () => stateContext.handleBoxClick());
+
   possibleButton.addEventListener('click', () => stateContext.changeState('possible'));
   impossibleButton.addEventListener('click', () => stateContext.changeState('impossible'));
-  
+
 
 
